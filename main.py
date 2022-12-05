c5740e20290807789aac4655427a775d3572c365
ngtuetam <tuetam011203@gmail.com>
2022-12-05 20:56:32 +0700

    fix: handle POST requests and return the results

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask, flash, request, redirect, url_for
 from flask_cors import CORS, cross_origin
-from predict import predict
+from predictimg import predict
 import cv2 as cv
 import numpy as np
-from tensorflow.keras import preprocessing
+from keras.utils import load_img
 
-
-# Khởi tạo Flask Server Backend
 app = Flask(__name__)
 
 # Apply Flask CORS
@@ -17,19 +15,20 @@ app.config['UPLOAD_FOLDER'] = ''
 
 @app.route('/', methods=['POST', 'GET'])
 @cross_origin(origin='*')
-def home():
+def process():
     if request.method == 'POST':
-        image = request.files['file']
-        file_bytes = np.fromstring(image, np.uint8)
-        img = cv.imdecode(file_bytes, cv.IMREAD_UNCHANGED)  
-        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        res = predict(img)
+        img = request.files['file']
+        img.save('image.jpg')
+        path ="image.jpg"
+        image = load_img(path, target_size=(224,224))
+        res = predict(image)
         return res
     if request.method == 'GET':
         return 'get'
     return ''
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=4000)
 
-# Start Backend
 if __name__ == '__main__':
     app.run()
--- predictimg.py
+++ predictimg.py
@@ -1,36 +1,39 @@
-from tensorflow.keras.models import load_model
-from tensorflow.keras import preprocessing
-import tensorflow as tf
-from argparse import ArgumentParser
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import numpy as np
 import pickle
 import cv2 as cv
-
-
-
-
+import matplotlib.pyplot as plt
+from keras.models import load_model
+from keras.utils import img_to_array, load_img
+from keras import preprocessing
+import tensorflow as tf
+from argparse import ArgumentParser
+from sklearn.preprocessing import LabelEncoder
+import glob
+plt.switch_backend('agg')
 # if __name__ == "__main__":
 def predict(image):
     # parser = ArgumentParser()
     # parser.add_argument("--test-file-path", type=str, required=True)
     # parser.add_argument("--model-path", default="best_model.h5", type=str)
     # parser.add_argument("--class-names-path", default='class_names.pkl', type=str)
-   
+
     # args = parser.parse_args()
-    
+
     class_names_path = "class_names.pkl"
     model_path = "checkpoints/best_model.h5"
 
     # print('Predict using ResNet model for test file path {0}'.format(args.test_file_path)) # fix
-    print('Predict using ResNet') # fix
+    print('Predict using ResNet')  # fix
     print('===============================================================================')
 
     # Loading class names
     # with open (args.class_names_path, 'rb') as fp:
     #   class_names = pickle.load(fp)
 
-    with open (class_names_path, 'rb') as fp:
-      class_names = pickle.load(fp)
+    with open(class_names_path, 'rb') as fp:
+        class_names = pickle.load(fp)
 
     # Loading model
     # model=load_model(args.model_path)
@@ -39,14 +42,17 @@ def predict(image):
     # Load test images
     # image = preprocessing.image.load_img(args.test_file_path, target_size=(224,224))
     # image = preprocessing.image.load_img(image, target_size=(224,224))
-    input_arr = preprocessing.image.img_to_array(image)/225
+    input_arr = img_to_array(image)/225
     x = np.expand_dims(input_arr, axis=0)
 
     predictions = model.predict(x)
-    label=np.argmax(predictions)
+    label = np.argmax(predictions)
+    
     print('Result: {}'.format(class_names[label]))
+    return class_names[label]
 
 # if __name__ == "__main__":
-#   path = "/home/ngtuetam/workspace/resnet/test_img/corgi.JPG"
-#   image = preprocessing.image.load_img(path, target_size=(224,224))
+#   path = "test_img/corgi.JPG"
+# #   image = preprocessing.image.load_img(path, target_size=(224,224))
+#   image = load_img(path, target_size=(224,224))
 #   predict(image)
